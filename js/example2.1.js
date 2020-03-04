ae04f3b20e0bb86c7e7ad9bf744049fd847de559
tooobening <48101049+tooobening@users.noreply.github.com>
2020-03-04 16:58:02 -0600

    sequence

---
+++ js/example2.1.js
@@ -0,0 +1,40 @@
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "Pop_2015";
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 /* Map of GeoJSON data from MegaCities.geojson */
 //declare map var in global scope
 var mymap;
+var minValue;
 
-//function to instantiate the Leaflet map
+//step 1 create map
 function createMap(){
 
     //add OSM base tilelayer
@@ -18,59 +19,166 @@ function createMap(){
     }).addTo(mymap);
 
     //call getData function
-    getData();
+    getData(mymap);
+};
+
+//Step 2: Import GeoJSON data
+function getData(mymap){
+    //load the data
+    $.ajax("data/AmtrakStations.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create an attributes array
+            var attributes = processData(response);
+            
+            calcMinValue(response);
+            createPropSymbols(response, attributes);
+            createSequenceControls(attributes);
+
+        }
+    });
+};
+
+function calcMinValue(data){
+     
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+         //loop through each year
+         for(var year = 2012; year <= 2018; year+=1){
+               //get population for current year
+              var value = city.properties["PASS_"+ String(year)];
+              //add value to array
+              allValues.push(value);
+          }
+    }
+    
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    
+    //Flannery Appearance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
 };
 
 //Step 3: Add circle markers for point features to the map
 function createPropSymbols(data){
-    //Step 4. Determine the attribute for scaling the proportional symbols
-    var attribute = "PASS_2019";
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(mymap);
+}
+
+
+
+
+
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng,attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    //check
+    console.log(attribute);
+
     //create marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800",
-        color: "#000",
+    var options = {
+        fillColor: "#ffffcc",
+        color: "#001",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
     };
 
-    //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]); //force-type any string values to numbers
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
 
-            //examine the attribute value to check that it is correct
-            console.log(feature.properties, attValue);
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
 
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(mymap);
-};
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
 
-//Step 2: Import GeoJSON data
-function getData(){
-    //load the data
-    $.getJSON("data/AmtrakStations.geojson", function(response){
-            //call function to create proportional symbols
-            createPropSymbols(response);
+    //build popup content string
+    var popupContent = "<p><b>Station Name:</b> " + feature.properties.STNNAME + "</p>";
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1]; //extract year after "_"
+    popupContent += "<p><b>Passenger in " + year + ":</b> " + feature.properties[attribute] + " thousand</p>";
+
+    //bind the popup to the circle marker
+
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius) //offset the popup based on its radius not to cover the proportional symbol
     });
+    
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+
+//-----------------Panel---------------------//
+//Step 1: Create new sequence controls
+function createSequenceControls(){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+    
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    $('#panel').append('<button class="step" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="step" id="forward">Forward</button>');
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        console.log(attribute)
+        // if (attribute[1:3] === "PASS"){
+        console.log(attribute[1,4])
+        }
+        // if (attribute.indexOf("PASS") > -1){
+        //     attributes.push(attribute);
+        // };
+    // };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
 };
 
 
 
 
+$(document).ready(createMap);
 
 
 
 
-
-
-
-// //function to retrieve the data and place it on the map
-// //Rather than having to create the data in the script, we bring in our external data and pass it directly to L.geoJson()
+//function to retrieve the data and place it on the map
+//Rather than having to create the data in the script, we bring in our external data and pass it directly to L.geoJson()
 // function getData(){
 //     //load the data
 //     $.getJSON("data/AmtrakStations.geojson", function(response){
@@ -121,5 +229,6 @@ function getData(){
 //         });
 // };
 
-// $(document).ready(createMap);
 
+
+// $(document).ready(createMap);
