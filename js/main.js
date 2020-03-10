37bafd86d7a64416e56d95888b37779d4dc8ac6b
tooobening <48101049+tooobening@users.noreply.github.com>
2020-03-10 03:59:39 -0600

    0310

--- js/main.js
+++ js/main.js
@@ -265,10 +265,6 @@ function updatePropSymbols(attribute){
 //     });
 // // };
 
-
-// function updateLegend(attribute){
-    
-// };
 //-----------------/Panel---------------------//
 //-----------------legend---------------------//
 // function createLegend(map, attributes){
@@ -327,41 +323,6 @@ function updatePropSymbols(attribute){
 
 // };
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //-------------OOP---------------//
 function PopupContent(properties, attribute){
     this.properties = properties;
@@ -391,69 +352,4 @@ function processData(data){
     return attributes;
 };
 //-------------/Process data, Return attrubutes arrays-----------------------//
-
-
-
-
-
-
 $(document).ready(createMap);
-
-
-
-
-//function to retrieve the data and place it on the map
-//Rather than having to create the data in the script, we bring in our external data and pass it directly to L.geoJson()
-// function getData(){
-//     //load the data
-//     $.getJSON("data/AmtrakStations.geojson", function(response){
-//             //create a Leaflet GeoJSON layer and add it to the map
-//             // L.geoJson(response).addTo(mymap);
-
-//             //Convert GeoJSON points into Leaflet layers -> showed as a circle marker
-//             var geojsonMarkerOptions = {
-//                 radius: 10,
-//                 fillColor: "#ffffcc", //Yellow
-//                 color: "#000",
-//                 weight: 1,
-//                 opacity: 1,
-//                 fillOpacity: 0.8
-//             };
-//             L.geoJson(response, {
-//                 pointToLayer: function (feature, latlng) {
-//                     return L.circleMarker(latlng, geojsonMarkerOptions);
-//                 }
-//             }).addTo(mymap);
-
-
-//             //Pop up selected feature
-//              L.geoJson(response, {
-//                 onEachFeature: function (feature,layer) {
-//                     var popupContent = ""; //define the string type
-//                     if (feature.properties) {
-//                         for (var property in feature.properties) {
-//                             //select specific properties to be included in the popup window
-//                             if (property === 'STNNAME' || 
-//                             property ==="PASS_2012" || 
-//                             property ==="PASS_2013" || 
-//                             property ==="PASS_2014" || 
-//                             property ==="PASS_2015" || 
-//                             property ==="PASS_2016" || 
-//                             property ==="PASS_2017" || 
-//                             property ==="PASS_2018" ){
-//                                 popupContent += "<p>" + property + ": " + feature.properties[property]+ "</p>";   
-//                             }                        
-//                         }
-//                         popupContent += "Unit: per thousand passengers" // this line might be deleted later for better presentation
-//                         return layer.bindPopup(popupContent)
-//                     }
-
-
-//                 }
-//              }).addTo(mymap);
-//         });
-// };
-
-
-
-// $(document).ready(createMap);
